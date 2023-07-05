fb11f64da17985efefbd957a5df7bc05106967af
Mishi <106777360+Miishri@users.noreply.github.com>
2023-07-05 16:53:57 +0200

    complete 1.7, 1.8, 1.9, 1.10, 1.11*

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,42 +1,88 @@
-import { useState } from 'react'
+import { useDebugValue, useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [percent, setPercent] = useState(0)
 
   const handleClickGood = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
+    updateStatistics()
   }
 
   const handleClickNeutral = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    updateStatistics()
   }
 
   const handleClickBad = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
+    updateStatistics()
   }
 
+  const incrementTotal = () => {
+    const updatedTotal = total + 1
+    setTotal(updatedTotal)
+  }
+ 
+  const handleUpdateAverage = () => {
+    let updatedAverage = (good * 1 + neutral * 0 + bad * -1) / total
+    if (isNaN(updatedAverage)) {
+      updatedAverage = 0
+    }
+    setAverage(updatedAverage)
+    console.log("average", updatedAverage)
+  }
 
+  const handleUpdatePercentage = () => {
+    let updatedPercent = (good/total) * 100
+    if (isNaN(updatedPercent)) {
+      updatedPercent = 0
+    }
+    setPercent(updatedPercent)
+    console.log("percent")
+  }
+
+  const updateStatistics = () => {
+    incrementTotal()
+    handleUpdateAverage()
+    handleUpdatePercentage()
+  }
 
   const course = {
     name: 'Give Feedback.',
+    title: 'Statistics',
     parts: [
       {
-        name: 'good',
+        name: 'Good',
         value: good
       },
       {
-        name: 'neutral',
+        name: 'Neutral',
         value: neutral
       },
       {
-        name: 'bad',
+        name: 'Bad',
         value: bad
+      },
+      {
+        name: 'Total',
+        value: total
+      },
+      {
+        name: 'Tverage',
+        value: average
+      },
+      {
+        name: 'PositivePercent',
+        value: percent
       }
     ]
   }
@@ -52,7 +98,6 @@ const App = () => {
   }
 
   const Header = ({course}) => {
-    console.log(course)
     return (
       <div>
         <h1>{course}</h1>
@@ -61,31 +106,82 @@ const App = () => {
   }
 
   const Part = (props) => {
-    console.log(props.name + " " + props.exercises)
     return (
-      <p>
-        {props.name} {props.exercises}
-      </p>
+      <tr>
+        <td>
+          {props.name}
+        </td>
+        <td>
+          {props.value}
+        </td>
+      </tr>
     )
   }
 
-  const Content = (props) => {
-    console.log(props)
+  const Votes = ({parts}) => {
+    const [good, neutral, bad] = parts
+
     return (
-      <div>
-        <Part name={props.parts[0].name} exercises={props.parts[0].value} />
-        <Part name={props.parts[1].name} exercises={props.parts[1].value} />
-        <Part name={props.parts[2].name} exercises={props.parts[2].value} />
-      </div>
+      <>
+        <Part name={good.name} value={good.value} />
+        <Part name={neutral.name} value={neutral.value} />
+        <Part name={bad.name} value={bad.value} />
+      </>
     )
   }
 
-  const Total = (props) => {
-    let total = props.parts[0].value + props.parts[1].value + props.parts[2].value
+  const Total = ({total}) => {
     return (
-      <div>
-        <p>Number of exercises {total}</p>
-      </div>
+      <tr>
+        <td>Total</td>
+        <td>{total[3].value}</td>
+      </tr>
+    )
+  }
+
+  const Average = ({average}) => {
+    return (
+      <tr>
+        <td>Average</td>
+        <td>{average[4].value} </td>
+      </tr>
+    )
+  }
+
+  const Percent = ({percent}) => {
+    return (
+      <tr>
+        <td>Percent</td>
+        <td>{percent[3].value} %</td>
+      </tr>
+    )
+  }
+
+  const Statistics = ({parts}) => {
+    if (parts[0].value || parts[1].value || parts[3].value) {
+
+      return (
+          <table>
+            <tbody>
+              <tr>
+                <th>
+                  <h1>Statistics</h1>
+                </th>
+              </tr>
+              <>
+                <Votes parts={parts} />
+                <Total total={parts} />
+                <Average average={parts} />
+                <Percent percent={parts} />
+              </>
+            </tbody>
+          </table>
+      )
+
+    }
+
+    return (
+      <p>No feedback given.</p>
     )
   }
 
@@ -93,9 +189,7 @@ const App = () => {
     <div>
       <Header course={course.name} />
       <Buttons />
-      <Header course={"Statistics"}/>
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Statistics parts={course.parts} />
     </div>
   )
 }
