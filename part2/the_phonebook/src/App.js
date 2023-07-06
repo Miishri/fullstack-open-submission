ac318b61b384b2f7c13aefba4117b1c497010caa
Mishi <106777360+Miishri@users.noreply.github.com>
2023-07-06 20:52:20 +0200

    complete 2.8

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -4,30 +4,35 @@ import { useState } from 'react'
 const Person = ({person}) => {
   return (
     <p>
-      {person.name}
+      {person.name} {person.phone}
     </p>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Arto 2342342' }
+    { name: 'Arto Hellas',
+      phone: 1231312
+    }
   ]);
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState(0);
 
   const addNewPerson = (event) => {
     event.preventDefault();
     const person = {
       name: newName,
+      phone: newPhone
     };
 
     if (newName !== '') {
-      if (personExists(person)) {
+      if (personExists(person).length) {
         alert(`${person.name} is already added to phonebook`)
+        console.log(personExists(person))
       }else {
         setPersons(persons.concat(person));
         setNewName('');
+        setNewPhone(0);
       }
     }
     //debug
@@ -38,6 +43,11 @@ const App = () => {
     console.log("In handle person change:", event.target.value);
     setNewName(event.target.value);
   };
+
+  const handlePhoneChange = (event) => {
+    console.log("In handle phone change:", event.target.value);
+    setNewPhone(event.target.value);
+  }
 
   function personExists(person){
     return persons.filter(p => p.name === person.name);
@@ -52,6 +62,12 @@ const App = () => {
           name: <input
             value={newName}
             onChange={handlePersonChange}
+          />
+        </div>
+        <div>
+          phone: <input
+            value={newPhone}
+            onChange={handlePhoneChange}
           />
         </div>
         <div>
