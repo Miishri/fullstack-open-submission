1fb261757c9967084376e42695f101896417317e
Mishi <106777360+Miishri@users.noreply.github.com>
2023-07-16 14:04:27 +0200

    complete 3.1

--- part3/testing/index.js
+++ part3/testing/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 
 let notes = [
@@ -29,14 +30,37 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)  
-    const note = notes.find(note => note.id === id)
-    if (note) {    
-        response.json(note)  
-    } else {    
-        response.status(404).end()  
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
     }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -48,5 +72,5 @@ app.delete('/api/notes/:id', (request, response) => {
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${d}`)
 })
